a8609822df086ffd8d16015855b24c944fecc03d
Carlos <ocariocawebdesign@gmail.com>
2019-11-23 19:18:48 -0300

    Novas alterações.

--- cadastro/conexao.php
+++ cadastro/conexao.php
@@ -1,10 +1,10 @@
 <?php
 
 
-$servidor = "";
-$usuario = "";
-$senha = "";
-$dbname = "";
+$servidor = "br1014.hostgator.com.br";
+$usuario = "mminfo53_inf";
+$senha = "J@DUMC6[Fo;Z";
+$dbname = "mminfo53_inf";
 
 
 //criar conexao
