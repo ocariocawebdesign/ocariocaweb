2c4302fa892c4e33f9ab16ff02b5620786eec7eb
Carlos <ocariocawebdesign@gmail.com>
2019-11-23 18:33:18 -0300

    Colocação da pasta cadastro.

---
+++ cadastro/cadastro.php
@@ -0,0 +1,196 @@
+<?php
+//https://www.youtube.com/watch?v=4tBeeMcw2sM&app=desktop
+
+session_start();
+?>
+
+
+<!DOCTYPE HTML>
+<!--Site: Carlos carlos Abreu ocariocawebdesign-->
+<html lang="pt-br">
+
+    <head>
+        <title>O Carioca Web Design</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+        <meta name="O Carioca Web" content=" Web design, html, css, desenvolvimento de sites">
+        <meta name="description" content="Página de contato O Carioca Web Design." />
+
+        <meta property="og:locale" content="pt_BR">
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="https://ocariocawebdesign.github.io/ocariocaweb/">
+        <meta property="og:title" content="O Carioca Web Design">
+        <meta property="og:site_name" content="O Carioca Web Design">
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="200" />
+        <meta property="og:image:height" content="132" />
+        <meta property="og:image" content="https://ocariocawebdesign.github.io/ocariocaweb/images/nova-logo-caricatura.png">
+
+        <!--twitter card-->
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@ocariocacawebdesign" />
+        <meta name="twitter:creator" content="@ocariocawebdesign" />
+        <meta property="og:url" content="https://ocariocawebdesign.github.io/ocariocaweb/" />
+        <meta property="og:title" content="A Twitter O Carioca Web Design" />
+        <meta property="og:description" content="Página de contato O Carioca Web design, html, css, desenvolvimento de sites." />
+        <meta property="og:image" content="https://ocariocawebdesign.github.io/ocariocaweb/images/nova-logo-caricatura.png" />
+
+
+
+
+
+
+  <!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous"/>
+        <link rel="stylesheet" href="assets/css/main.css" />
+        <link rel="stylesheet" href="css/estilo2.css">
+        <link rel="stylesheet" href="assets/css/bootstrap.css" />
+        <link rel="stylesheet" href="css/animate.css">
+
+    </head>
+
+
+    <body>
+
+
+        <!--GoogleAnalytics-->
+        <script>
+            (function (i, s, o, g, r, a, m) {
+                i['GoogleAnalyticsObject'] = r;
+                i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+                a = s.createElement(o),
+                        m = s.getElementsByTagName(o)[0];
+                a.async = 1;
+                a.src = g;
+                m.parentNode.insertBefore(a, m)
+            })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+            ga('create', 'UA-16045147-11', 'auto');
+            ga('send', 'pageview');
+        </script>
+
+        <div class="container">
+            <!--GoogleAnalytics-->
+
+            <!-- Header -->
+            <header id="header">
+
+      <!--	<input type="button" onclick="funcao1()" value="Oi! Clique aqui!" />-->
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                        <img class="img-responsive animated fadeInLeft" src="images/logo-only.png" alt="O carioca Web Design">
+                    </div>
+
+                    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                        <img class="img-responsive animated fadeInRight" src="images/nova-logo-caricatura.png" alt="O carioca Web Design">
+                    </div>
+                </div>
+
+            </header>
+
+            <br>
+            <br>
+            <div class="row">
+                <div style="margin-bottom:2%;" class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                    <button type="button" class="button-job btn-lg">Trampos</button>
+                    <br>
+                </div>
+
+                <div style="margin-bottom:2%;" class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                    <button type="button" class="button-job btn-lg">Wordpress CMS</button>
+                    <br>
+                </div>
+
+                <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                    <button type="button" class="button-job btn-lg">Sites em HTML|CSS</button>
+                    <br>
+                </div>
+
+            </div>
+
+            <br>
+
+
+            <div id="box-conteudo-1">
+
+               
+                <main>
+
+                    
+
+                    <style>
+                        placehoder {
+                            color: #333;
+
+                        }
+
+                        input[type=text] {
+
+                            width: 30rem;
+
+                        }
+
+                        input[type=email] {
+
+                            width: 30rem;
+
+                        }
+                    </style>
+
+
+                    <form method="POST" action="processa.php">
+                        <label style="color:#333;">Nome:</label>
+                        <input type="text" name="nome" placeholder="Digite seu nome completo">
+
+                        <label style="color:#333;">email:</label>
+                        <input type="email" name="email" placeholder="Digite seu email">
+
+
+                        <input type="submit" name="botao" value="cadastrar">
+
+                    </form>
+                    <?php
+                    if (isset($_SESSION['msg'])) {
+
+                        echo $_SESSION['msg'];
+                        unset($_SESSION['msg']);
+                    }
+                    ?>
+
+                    <br/>
+                    <br/>
+                   
+                </main>
+
+            </div>
+
+
+
+            <div class="container">
+
+                <div class="row">
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                        <!--Botão facebook-->
+                        <a style="text-decoration:none; border:0px;" href="http://m.me/ocariocawebdesign" target="_blank"><img src="images/messenger.png" alt="Botão do messenger" title="Clique aqui" />
+                            <h4 style="color:#333;">Mande uma mensagem!</h4>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+
+                <a class="twitter-timeline" data-theme="light" href="https://twitter.com/CarlosA93259670">Tweets by O
+                    Carioca</a>
+                <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+
+            </div>
+
+
+    </body>
+
+</html>
