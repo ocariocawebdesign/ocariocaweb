14213df3712eef0d616e72450e598582d3a28b13
Carlos <ocariocawebdesign@gmail.com>
2020-06-12 18:58:03 -0300

    Novas alterações e inclusão do scripts para usar api rest wp. 12/06/2020

---
+++ js/get-post.js
@@ -0,0 +1,7 @@
+//Sua API
+let postsApi = 'https://ocariocawebdesign.com/wp-json/wp/v2/posts'
+
+let posts = Http
+posts.inicia(postsApi)
+
+
---
+++ modules/Http.js
@@ -0,0 +1,26 @@
+const Http = {
+    inicia: (postsAPI) => Http._pega(postsAPI),
+    _pega: (postsAPI) => {
+        fetch(postsAPI, { method: 'get' })
+            .then(res => res.json())
+            .then(posts => Http._monta(posts), err => {
+                let template = `
+				<span style="color:red">
+					Esse servidor não fornece permissão para consumo de sua API
+				</span>
+				`
+                document.querySelector('.posts').innerHTML = template
+            })
+    },
+    _monta: (posts) => {
+        posts.map(post => {
+            let template = `
+				<div class="post">
+					<h1>${post.title.rendered}</h1>
+					<p>${post.content.rendered}</p>
+				</div>
+			`
+            document.querySelector('.posts').innerHTML += template
+        })
+    }
+}
